578a88d8f187decdba32ed202c7ec2f6e8cd29a2
Учкин Андрей Алексеевич <uchkin.aa0162@okeit.edu>
2021-01-18 15:06:59 +0500

    Initial commit

---
+++ salonUchkin/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace salonUchkin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using(Context db = new Context())
+            {
+                ClientsDG.ItemsSource = db.Client.ToList();
+              
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 f = new Window1();
+            f.Show();
+            this.Close();
+        }
+        private List<Client> Search(string searchQuery)
+        {
+            List<Client> _clients = new List<Client>();
+            using (Context db = new Context())
+            {
+                foreach (Client client in db.Client.ToList())
+                {
+                    if ($"{client.ID},{client.FirstName},{client.LastName},{client.Patronymic},{client.Birthday},{client.RegistrationDate},{client.Email},{client.Phone},{client.GenderCode},{client.PhotoPath}".IndexOf(searchQuery) >= 0)
+                    {
+                        _clients.Add(client);
+                    }
+                }
+
+            }
+            return _clients;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (Context db = new Context())
+            {
+                if (ClientsDG.SelectedItems.Count > 0)
+                    for (int i = 0; i < ClientsDG.SelectedItems.Count; i++)
+                    {
+                        Client client = ClientsDG.SelectedItem as Client;
+                        if (client != null)
+                        {
+                            db.Entry(client).State = EntityState.Deleted;
+                            //db.Client.Remove(client);
+                            db.SaveChanges();
+                            ClientsDG.ItemsSource = db.Client.ToList();
+                            MessageBox.Show("Данные удалены", "Уведомление");
+                        }
+                    }
+            } 
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            List<Client> clients = Search(poisk.Text);
+            ClientsDG.ItemsSource = clients;
+        }
+    }
+    }
+
---
+++ salonUchkin/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace salonUchkin
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow f = new MainWindow();
+            f.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (Context db = new Context())
+            {
+                Client client = new Client { FirstName = TB1.Text, LastName = TB2.Text, Patronymic = TB3.Text, Birthday = Convert.ToDateTime(TB4.Text), RegistrationDate = Convert.ToDateTime(TB5.Text), Email = TB6.Text, Phone = TB7.Text, GenderCode = TB8.Text, PhotoPath = TB9.Text };
+                db.Client.Add(client);
+                db.SaveChanges();
+            }
+        }
+    }
+}
